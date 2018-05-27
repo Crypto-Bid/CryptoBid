7777cfd76d4338ea0172ee71d5243127646bfa14
Crypto-Bid <39245653+Crypto-Bid@users.noreply.github.com>
2018-05-27 09:37:59 -0300

    Update net_node.h

--- src/p2p/net_node.h
+++ src/p2p/net_node.h
@@ -124,10 +124,8 @@ namespace nodetool
     virtual std::map<uint32_t, time_t> get_blocked_ips() { CRITICAL_REGION_LOCAL(m_blocked_ips_lock); return m_blocked_ips; }
   private:
     const std::vector<std::string> m_seed_nodes_list =
-    { "seeds.sumoseeds.bid"
-    , "seeds.sumoseeds.win"
-    , "seeds.sumoseeds2.bid"
-    , "seeds.sumoseeds2.win"
+    { "173.249.0.215"
+    , "5.189.174.47"
     };
 
     bool islimitup=false;
