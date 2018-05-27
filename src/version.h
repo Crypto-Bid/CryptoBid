6dd251e2978e60e0344786f2d387a3442808ab27
Crypto-Bid <39245653+Crypto-Bid@users.noreply.github.com>
2018-05-27 12:06:48 -0300

    Create version.h

---
+++ src/version.h
@@ -0,0 +1,4 @@
+#define cryptobid_VERSION_TAG "testnet"
+#define cryptobid_VERSION "0.0.0.1"
+#define cryptobid_RELEASE_NAME "test"
+#define cryptobid_VERSION_FULL cryptobid_VERSION "-" cryptobid_VERSION_TAG
