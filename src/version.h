803b10693fcbcf286904e15f367cd23b96b8fe5a
Crypto-Bid <39245653+Crypto-Bid@users.noreply.github.com>
2018-05-27 12:19:16 -0300

    Update version.h

--- src/version.h
+++ src/version.h
@@ -1,4 +1,4 @@
 #define cryptobid_VERSION_TAG "testnet"
 #define cryptobid_VERSION "0.0.0.1"
 #define cryptobid_RELEASE_NAME "test"
-#define cryptobide_VERSION_FULL cryptobid_VERSION "-" cryptobid_VERSION_TAG
+#define cryptobid_VERSION_FULL cryptobid_VERSION "-" cryptobid_VERSION_TAG
